362de8a2354384acc50d2b5bd6206801e0f400e0
Igor <iantipin@mail.ru>
2020-04-04 14:58:04 +0300

    dev

---
+++ src/Orc/Orc.Common/Enums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orc.Common
+{
+	public static class Enums
+	{
+		/// <summary>
+		/// Enumeration of standard directions
+		/// </summary>
+		public enum WorldDirection
+		{
+			/// <summary>
+			/// North
+			/// </summary>
+			N = 0,
+			
+			/// <summary>
+			/// East
+			/// </summary>
+			E,
+			
+			/// <summary>
+			/// South
+			/// </summary>
+			S,
+			
+			/// <summary>
+			/// West
+			/// </summary>
+			W
+		}
+	}
+}
