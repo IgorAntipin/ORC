afd3c80f563038b5044266554df1f1cdbee2316b
Igor <iantipin@mail.ru>
2020-04-04 14:34:40 +0300

    initial

---
+++ src/Orc/Orc.Common/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orc.Common
+{
+	public static class Constants
+	{
+
+		/// <summary>
+		/// The maximum number of commands a robot can execute in one task
+		/// </summary>
+		public const int MAX_COMMANDS = 10000;
+
+		/// <summary>
+		/// The maximum number of steps a robot can take in one direction
+		/// </summary>
+		public const int MAX_STEPS = 100000;
+	}
+}
---
+++ src/Orc/OrcProto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrcProto
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Orc prototype");
+		}
+	}
+}
