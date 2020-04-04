cea0b81c90b524ed10cd9a8d837f66340f26f808
Igor <iantipin@mail.ru>
2020-04-04 18:10:00 +0300

    added basic console app - OrcProto; added nuget packages, Autofac DI container, Serilog;

---
+++ src/Orc/OrcProto/ContainerConfig.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrcProto
+{
+	public static class ContainerConfig
+	{
+
+		public static IContainer GetContainer()
+		{
+			var builder = new ContainerBuilder();
+
+
+
+			return builder.Build();
+		}
+	}
+}
--- src/Orc/OrcProto/Program.cs
+++ src/Orc/OrcProto/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
+﻿using Autofac.Extensions.DependencyInjection;
+using Orc.Infrastructure.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Serilog;
+using System.Threading.Tasks;
 
 namespace OrcProto
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static async Task<int> Main(string[] args)
 		{
 			Console.WriteLine("Orc prototype");
+
+			ConfigureLogger();
+
+			var container = ContainerConfig.GetContainer();
+
+			var serviceProvider = new AutofacServiceProvider(container);
+
+			try
+			{
+				var _robot = serviceProvider.GetService<IRobot>();
+				await _robot.DoJobAsync();
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "ORC Proto crushed due to a critical error.");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		private static void ConfigureLogger()
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Debug()
+				.WriteTo.File("OrcProto.log")
+				.CreateLogger();
+
 		}
 	}
 }
